04b67762114798a80611afe973ceda109d50662c
Rishabh <92223440+LightClover-Dev@users.noreply.github.com>
2026-06-22 17:13:58 +0530

    Refactor linked list functions for safety and memory management
    Updated push function to handle memory allocation errors and changed new_data type from char to int. Added freeList function to prevent memory leaks.

--- code/data_structures/linked_list/linked_list/singly_linked_list.cpp
+++ code/data_structures/linked_list/linked_list/singly_linked_list.cpp
@@ -1,47 +1,81 @@
-// C++ program to print reverse of a linked list
-#include<stdio.h>
-#include<stdlib.h>
-  
+#include <stdio.h>
+#include <stdlib.h>
+
 struct Node
 {
     int data;
     struct Node* next;
 };
-  
+
+/*
+ * Prints linked list in reverse order using recursion.
+ * Does not modify the original list.
+ */
 void printReverse(struct Node* head)
 {
-     
     if (head == NULL)
-       return;
- 
-    
+        return;
+
     printReverse(head->next);
- 
-        printf("%d  ", head->data);
+
+    printf("%d ", head->data);
 }
-  
-void push(struct Node** head_ref, char new_data)
+
+/*
+ * CHANGE 1:
+ * Changed new_data type from char to int
+ * because Node::data is an int.
+ */
+void push(struct Node** head_ref, int new_data)
 {
-  
-    struct Node* new_node =
-            (struct Node*) malloc(sizeof(struct Node));
-  
-      new_node->data  = new_data;
-  
-  
-    new_node->next = (*head_ref);   
-  
-      (*head_ref)    = new_node;
-} 
-  
+    struct Node* new_node = malloc(sizeof(struct Node));
+
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->data = new_data;
+    new_node->next = *head_ref;
+    *head_ref = new_node;
+}
+
+/*
+ * CHANGE 2:
+ * Added function to free allocated memory
+ * and prevent memory leaks.
+ */
+void freeList(struct Node* head)
+{
+    while (head != NULL)
+    {
+        struct Node* temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
 int main()
 {
-    struct Node* head = NULL;    
+    struct Node* head = NULL;
+
     push(&head, 4);
     push(&head, 3);
     push(&head, 2);
     push(&head, 1);
-   
+
+    printf("Linked List in Reverse:\n");
     printReverse(head);
+
+    printf("\n");
+
+    /*
+     * CHANGE 3:
+     * Free dynamically allocated memory
+     * before program exits.
+     */
+    freeList(head);
+
     return 0;
 }
